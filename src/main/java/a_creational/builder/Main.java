2d1ec855c403030dd98c1d0d3fba6834e5cd7122
Pawel Janusz <marek80k@wp.pl>
2021-11-12 20:42:57 +0100

    builder

--- src/main/java/a_creational/builder/Main.java
+++ src/main/java/a_creational/builder/Main.java
@@ -7,14 +7,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        House house = new House.HouseBuilder()
+        House smallHouse = new House.HouseBuilder()
                 .buildWalls("20 walls")
                 .buildFloors("10 floors")
                 .buildDoors("5 doors")
                 .buildRooms("5 rooms")
                 .build();
 
-        System.out.println(house);
+        System.out.println("Small house: " + smallHouse);
+
+        House bigHouse = new House.HouseBuilder()
+                .buildWalls("30 walls")
+                .buildFloors("30 floors")
+                .buildDoors("10 doors")
+                .buildRooms("10 rooms")
+                .buildWindows("23 widows")
+                .buildRoof("1 roof")
+                .buildGarage("2 garage")
+                .build();
+
+        System.out.println("Big house: " + bigHouse);
+
     }
 
 }
